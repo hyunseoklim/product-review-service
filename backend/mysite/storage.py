0f4b8dca68a9098639be57cdd27e7bff2b258e09
Hyunseok Lim <andyy7919@gmail.com>
2026-04-07 15:11:50 +0900

    fix: add region to S3 storage URL

--- backend/mysite/storage.py
+++ backend/mysite/storage.py
@@ -6,9 +6,13 @@ class StaticStorage(S3Storage):
     location = "static"
     default_acl = None
     querystring_auth = False
+    region_name = os.getenv("AWS_S3_REGION_NAME", "ap-northeast-2")
+    custom_domain = f"{os.getenv('AWS_STORAGE_BUCKET_NAME_STATIC')}.s3.{os.getenv('AWS_S3_REGION_NAME', 'ap-northeast-2')}.amazonaws.com"
 
 class MediaStorage(S3Storage):
     bucket_name = os.getenv("AWS_STORAGE_BUCKET_NAME_MEDIA")
     location = "media"
     default_acl = None
     querystring_auth = False
+    region_name = os.getenv("AWS_S3_REGION_NAME", "ap-northeast-2")
+    custom_domain = f"{os.getenv('AWS_STORAGE_BUCKET_NAME_MEDIA')}.s3.{os.getenv('AWS_S3_REGION_NAME', 'ap-northeast-2')}.amazonaws.com"
